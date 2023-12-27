7d5d5f6f5025b3d156ac02afb4a0ab1cb3c34e09
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-28 00:17:31 +0200

    Добавьте файлы проекта.

---
+++ lab 4-2/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Введите размерность матрицы N:");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+
+Console.WriteLine("Введите элементы матрицы:");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("Исходная матрица:");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Зеркально отраженная матрица:");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[n - j - 1, n - i - 1] + " ");
+    }
+    Console.WriteLine();
+}
